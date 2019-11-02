6f3c68d73184a999cfc09e16ad3f16d83b0bef54
Niklas <me@sodapop.se>
2019-11-02 23:31:50 +0100

    nil host

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :chalmersfood, ChalmersfoodWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [scheme: "https", host: "mat.sodapop.se", port: 443],
+  url: [scheme: "https", host: nil, port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
