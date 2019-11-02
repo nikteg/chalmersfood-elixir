e964371b9abf52e040d5191f8dc9857f70c60480
Niklas <me@sodapop.se>
2019-11-02 23:26:43 +0100

    Don't force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,6 @@ use Mix.Config
 config :chalmersfood, ChalmersfoodWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "mat.sodapop.se", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
